af70c2349374af97ff2ac2c4e695f7c0e9879b41
simplelife81 <bhupendrakalerk@gmail.com>
2026-02-25 15:14:02 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+API_ID = int(os.getenv("API_ID", "123456"))
+API_HASH = os.getenv("API_HASH", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+
+OWNER_ID = int(os.getenv("OWNER_ID", "0"))
+CHANNEL_ID = int(os.getenv("CHANNEL_ID", "0"))
