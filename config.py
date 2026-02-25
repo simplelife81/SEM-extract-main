d66c260823df96180910a8a3c63cd573a8589b5b
simplelife81 <bhupendrakalerk@gmail.com>
2026-02-25 17:14:25 +0530

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-API_ID = int(os.getenv("API_ID", "123456"))
-API_HASH = os.getenv("API_HASH", "")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-
-OWNER_ID = int(os.getenv("OWNER_ID", "0"))
-CHANNEL_ID = int(os.getenv("CHANNEL_ID", "0"))
